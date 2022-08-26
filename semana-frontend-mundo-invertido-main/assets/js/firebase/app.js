c8b9cca1f367453e81fca0f0992f38501bd9ea45
Davi Pereira Barros <davipereirabp@gmail.com>
2022-08-26 19:44:23 -0300

    Completo

---
+++ semana-frontend-mundo-invertido-main/assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDpfYhmjeNqdbhsAM8ijG17IcBKUmNYjAw",
+    authDomain: "mundo-invertido-ae486.firebaseapp.com",
+    projectId: "mundo-invertido-ae486",
+    storageBucket: "mundo-invertido-ae486.appspot.com",
+    messagingSenderId: "324749860664",
+    appId: "1:324749860664:web:314f51cab779d4ff54abcd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export default app
